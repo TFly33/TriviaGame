a48f1087a0224b40df39e57cb67c166487981c94
Tommy Flynn <TFly.Flynn@gmail.com>
2019-10-24 20:08:52 -0400

    finally got the countdown function properly, with the timer only starting when the start button is clicked.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,29 +1,31 @@
-window.onload = function () {
-    var timer = 0;
-    var interval = "";
 
-    function start() {
-        var timer = 90
-        var interval = setInterval(function () {
-            timer--;
-            $(".timeRemaining").html(timer);
-            console.log(timer)
-            if (timer === 0) {
-                console.log("Time's Up");
-                clearInterval(interval);
-            }
+function start() {
+    var timer = 90
+    var interval = setInterval(function () {
+        timer--;
+        $(".timeRemaining").html(timer);
+        console.log(timer);
+        if (timer === 0) {
+            console.log("Time's Up");
+            // Don't want the timer to go past 0. Probably won't matter because we'll hide all of that anyway. 
+            clearInterval(interval);
+            // Let's place a "Time's Up" function, which we can define later. It will pop up with a new screen that lists the right and wrong answers. 
+            timesUp();
         }
-            , 1000);
     }
-    $(".start").on("click");
-    start();
+        , 1000);
 }
 
+$(".start").click(function(){
+    start();
+    console.log ("and go.");
+});
+
+
+// I need to create two arrays (I think). One array includes the correct answers, one includes the wrong answers. 
+
+// Then I can take the usersubmissions, and 
 
 
 
 
-
-
-
-// $("<button>").text("hello there");
